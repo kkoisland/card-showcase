a4ea5a35f4f927fb828193e05df388c4d70048c1
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2025-11-05 14:49:56 -0800

    feat: add loading spinner

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,6 +4,7 @@ const Card = () => {
 	const params = new URLSearchParams(location.search);
 	const src = params.get("src");
 	const [showIframe, setShowIframe] = useState(false);
+	const [loading, setLoading] = useState(true);
 
 	if (!src) return <p>No image source provided.</p>;
 
@@ -11,9 +12,17 @@ const Card = () => {
 		<button
 			type="button"
 			className="w-screen h-screen flex justify-center p-0 m-0 bg-transparent border-0"
-			onClick={() => setShowIframe(!showIframe)}
-			aria-label="toggle viewer"
+			onClick={() => {
+				setShowIframe(!showIframe);
+				setLoading(true);
+			}}
 		>
+			{loading && (
+				<div className="absolute flex items-center justify-center">
+					<div className="w-8 h-8 border-4 border-t-transparent rounded-full animate-spin" />
+				</div>
+			)}
+
 			{showIframe ? (
 				<>
 					<button
@@ -30,6 +39,7 @@ const Card = () => {
 						src={src}
 						title="Greeting Card"
 						className="w-full h-full border-none"
+						onLoad={() => setLoading(false)}
 					/>
 				</>
 			) : (
@@ -37,6 +47,7 @@ const Card = () => {
 					src={src}
 					alt="Greeting Card"
 					className="max-w-full max-h-full object-contain"
+					onLoad={() => setLoading(false)}
 				/>
 			)}
 		</button>
